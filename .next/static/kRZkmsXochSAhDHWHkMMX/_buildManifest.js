a887da28621ae9576da9e07c12157a26639ac5e8
유도건 <dgyoo@nextree.io>
2021-11-30 17:39:48 +0900

    default setting

---
+++ .next/static/kRZkmsXochSAhDHWHkMMX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-601c334056601a913688.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
